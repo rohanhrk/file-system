c35ba9a3d35aa8195ca0db1f81c5747c81ab3d62
ROHAN HAZARIKA <rohanhrk1999@gmail.com>
2021-03-03 23:15:03 +0530

    added

---
+++ commands/view.js
@@ -0,0 +1,87 @@
+let fs=require("fs");
+let path=require("path");
+function viewHelper(dirName, mode) {
+    if (mode == "tree") {
+        viewTree(dirName,"");
+    } else if (mode == "flat") {
+        viewFlat(dirName);
+    } else {
+        console.log("wrong mode  type help for commands");
+    }
+}
+module.exports = {
+    fn: viewHelper
+}
+
+// view flat
+function viewFlat(src) {
+    // check the given source is file or not 
+    let isFile = isFileOrNot(src);
+    if (isFile == true) {
+        //    if given src is file 
+        //    then print src
+        console.log(src + "*");
+    }
+    else {
+        // print src
+        console.log(src);
+        // content read from os
+        let fDirnames = readContent(src);
+        // recursion 
+        // console.log(fDirnames);
+        for (let i = 0; i < fDirnames.length; i++) {
+            let child = fDirnames[i];
+            //    good practice??
+            // let dirNamepath = src + "\\" + child;
+            let dirNamepath = path.join(src, child);
+            viewFlat(dirNamepath)
+        }
+    }
+}
+
+//view tree
+function viewTree(src, indent) {
+    //check given src is file or not
+    let isFile = isFileOrNot(src);
+    if (isFile == true) {
+        //if file 
+        //then print path of last name
+        console.log(indent, path.basename(src), "*");
+    }
+    else {
+        // print
+        console.log(indent, path.basename(src));
+        // content read from os
+        let fDirnames = readContent(src);
+        // recursion 
+        // console.log(fDirnames);
+        for (let i = 0; i < fDirnames.length; i++) {
+            let child = fDirnames[i];
+            //    good practice??
+            // let dirNamepath = src + "\\" + child;
+            let dirNamepath = path.join(src, child);
+            viewTree(dirNamepath, indent + "\t");
+        }
+    }
+}
+
+//check given path is file or not
+function isFileOrNot(src) {
+    return fs.lstatSync(src).isFile();
+}
+
+//read content 
+function readContent(src) {
+    return fs.readdirSync(src);
+}
+
+// folder
+        // activity
+            // commands
+                // * help.js
+                // * view.js
+                // * organize.js
+            // * mycli.js
+        // raw
+            // poc
+            // facts
